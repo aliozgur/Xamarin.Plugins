dea020d44d5f9e0484f190c17bc5b229a1f4ade7
Ali Özgür <aliozgur79@gmail.com>
2014-12-24 00:21:59 +0200

    Demo solution initial commit

---
+++ DeviceOrientationDemo/DeviceOrientationDemo/MainPage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Xamarin.Forms;
+using DeviceOrientation.Forms.Plugin.Abstractions;
+
+namespace DeviceOrientationDemo
+{
+    public class MainPage:ContentPage
+    {
+        IDeviceOrientation _deviceOrientationSvc;
+        private Label _label;
+
+        public MainPage()
+        {
+            _deviceOrientationSvc = DependencyService.Get<IDeviceOrientation>();
+            PrepareControls();
+        }
+
+        private void PrepareControls()
+        {
+           
+            _label = new Label
+            {
+                
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            _label.Text = String.Format("Hello, Forms! My initial orientation is {0}",_deviceOrientationSvc.GetOrientation());
+
+            Content = _label;
+           
+         
+
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            MessagingCenter.Subscribe<DeviceOrientationChangeMessage>(this, DeviceOrientationChangeMessage.MessageId, (message) =>
+                {
+                    HandleOrientationChange(message);
+                });
+        }
+
+        protected override void OnDisappearing()
+        {
+            MessagingCenter.Unsubscribe<DeviceOrientationChangeMessage>(this, DeviceOrientationChangeMessage.MessageId);
+            base.OnDisappearing();
+        }
+
+        private void HandleOrientationChange(DeviceOrientationChangeMessage mesage)
+        {
+            _label.Text = String.Format("Orientation changed to {0}", mesage.Orientation.ToString());
+        }
+    }
+}
+
---
+++ DeviceOrientationDemo/iOS/AppDelegate.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+
+using Xamarin.Forms;
+using DeviceOrientation.Forms.Plugin.iOS;
+
+namespace DeviceOrientationDemo.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : UIApplicationDelegate
+    {
+        UIWindow window;
+
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            Forms.Init();
+            DeviceOrientationImplementation.Init();
+
+            window = new UIWindow(UIScreen.MainScreen.Bounds);
+			
+            window.RootViewController = App.GetMainPage().CreateViewController();
+            window.MakeKeyAndVisible();
+			
+            return true;
+        }
+    }
+}
+
